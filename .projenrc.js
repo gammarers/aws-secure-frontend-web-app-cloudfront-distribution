8222e0a8cd8f921bfe6799d99c9d85690c80ec05
yicr <yicr@users.noreply.github.com>
2023-01-05 14:43:51 +0900

    chore: project created with projen

---
+++ .projenrc.js
@@ -0,0 +1,20 @@
+const { awscdk } = require('projen');
+
+const PROJECT_NAME = '@yicr/frontend-web-app-cloudfront-distribution';
+const PROJECT_DESCRIPTION = 'AWS CloudFront distribution for frontend web app (spa) optimized.';
+
+const project = new awscdk.AwsCdkConstructLibrary({
+  author: 'yicr',
+  authorAddress: 'yicr@users.noreply.github.com',
+  cdkVersion: '2.43.0',
+  defaultReleaseBranch: 'main',
+  name: PROJECT_NAME,
+  description: PROJECT_DESCRIPTION,
+  repositoryUrl: 'https://github.com/yicr/frontend-web-app-cloudfront-distribution.git',
+  keywords: ['aws', 'cdk', 'cloudfront', 'cdn', 'web', 'vue', 'angular', 'react'],
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
